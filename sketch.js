be5d62ee9524e64f5294e3186f70db33b146499f
Ryan Trammell <thetrammster11@gmail.com>
2025-06-24 11:19:18 -0700

    added rocks

--- sketch.js
+++ sketch.js
@@ -9,22 +9,36 @@ let speed = 5;
 
 
 let player;
+let rock1;
+let rock2;
+let rock3;
+let rock4;
+
+
+// Load assets
 function preload(){
     player = loadImage("images/NewCart.png");
+    rock1 = loadImage("image/Rock1.png");
+    //rocks[1] = loadImage("image/Rock2");
+    //rocks[2] = loadImage("image/Rock3");
+    //rocks[3] = loadImage("image/Rock4");
 
 }
 
 function setup() {
     createCanvas(600, 600);
+    image(rock1, 100, 100, 100, 100);
 }
 
 function draw() {
     background(220);
   
-  // draw moving character
+  // draw character
     fill(0, 0, 255);
     image(player, xpos, ypos, 100, 75);
-  
+    
+
+    // Player movement
     if (movingRight) {
         xpos += speed;
     }
@@ -37,11 +51,13 @@ function draw() {
     if (movingDown) {
         ypos += speed;
     }
+
+    //Makign the rocks fall
+    
+
+
 }
-  
-  
-
-
+//Player input
 function keyPressed() {
     if (key == 'w') {
         movingUp = true;
