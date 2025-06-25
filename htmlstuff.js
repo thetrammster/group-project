7bd3d984d73b7c8405e54ab75841704ace387bef
2ff215417 <welovedemarcuscousins@gmail.com>
2025-06-25 06:33:21 +0000

    smoll update to last commit

--- htmlstuff.js
+++ htmlstuff.js
@@ -1,3 +1,5 @@
 function Nextbutton() {
-    document.getElementById("2nd").innerHTML = "Allright, Now that you got the jist of it, lemme fill you in one something else. DO NOT, and I REPEAT. DO NOT TOUCH THE BOMBS!";
-}
+    document.getElementById("2nd").innerHTML = "Alright, Now that you got the jist of it, lemme fill you in one something else. DO NOT, and I REPEAT. <span style='color:red'><b>DO NOT TOUCH THE BOMBS!</b></span> Good luck!<br> <br>PS, its A to move left, and it is D to move right. <br> Press p to play";
+
+} 
+// Bassically changes the html inside my paragraph  with 2nd id
