cbb592b16efb307cc5226d63ef40817e4746cff1
2ff215417 <welovedemarcuscousins@gmail.com>
2025-06-25 06:03:58 +0000

    changes to intro

---
+++ htmlstuff.js
@@ -0,0 +1,3 @@
+function Nextbutton() {
+    document.getElementById("2nd").innerHTML = "Allright, Now that you got the jist of it, lemme fill you in one something else. DO NOT, and I REPEAT. DO NOT TOUCH THE BOMBS!";
+}
