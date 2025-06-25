50d814f26e0d8b0b8a2f2ef0ec19d02f390b4a12
2ff215417 <welovedemarcuscousins@gmail.com>
2025-06-25 16:51:42 +0000

    back button

--- htmlstuff.js
+++ htmlstuff.js
@@ -1,5 +1,15 @@
-function Nextbutton() {
-    document.getElementById("2nd").innerHTML = "Alright, Now that you got the jist of it, lemme fill you in one something else. DO NOT, and I REPEAT. <span style='color:red'><b>DO NOT TOUCH THE BOMBS!</b></span> Good luck!<br> <br>PS, its A to move left, and it is D to move right. <br> Press p to play";
+function Nextbutton() 
+{
 
+    document.getElementById("2nd").innerHTML = "Alright, Now that you got the jist of it, lemme fill you in one something else. DO NOT, and I REPEAT. <span style='color:red'><b>DO NOT TOUCH THE BOMBS!</b></span> Good luck!<br> <br>PS, its A to move left, and it is D to move right. <br> Press p to play";
 } 
+
+function Backbutton()
+{
+    document.getElementById("2nd").innerHTML = "asdasdas"
+}
+
+
+
+
 // Bassically changes the html inside my paragraph  with 2nd id
